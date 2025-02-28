31151270417c785bdd014ed99c4359404a798d46
Dawit Mellese <dawitm777@gmail.com>
2025-02-28 23:35:35 +0300

    simplify transaction data retrieval; remove unnecessary URL formatting

--- controllers/tnx.c.ts
+++ controllers/tnx.c.ts
@@ -4,8 +4,7 @@ import { geminiSummary } from "../utils/summary";
 
 export const getTnxDataByData = async (req: Request, res: Response) => {
   const { url } = req.params;
-  const formmatUrl = `https://etherscan.io/tx/${url}`;
-  const transactionData = await scrapeEtherscanTransaction(formmatUrl);
+  const transactionData = await scrapeEtherscanTransaction(url);
   const aiSummary = await geminiSummary(transactionData);
   res.json({ txData: transactionData, summary: aiSummary });
 };
